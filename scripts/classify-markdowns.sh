2ce9f8f9d3b9413482bb3525ffe7b671573b7eef
Ray Bunnage <bunnage.ray@gmail.com>
2025-03-10 17:16:54 -0700

    added new script that will do the work

---
+++ scripts/classify-markdowns.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# classify-markdowns.sh - A script for classifying markdown documents
+# Currently just prints "hello" to the console
+
+# Define colors for better readability
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+BOLD='\033[1m'
+NC='\033[0m' # No Color
+
+echo -e "${BOLD}${BLUE}=== Markdown Classifier ===${NC}\n"
+echo -e "${GREEN}hello${NC}"
+echo -e "\n${BOLD}${BLUE}=== End of Markdown Classifier ===${NC}" 
