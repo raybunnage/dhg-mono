64d15bb7e7b3fc881fecaeda02861aad5dc36e8a
Ray Bunnage <bunnage.ray@gmail.com>
2025-03-29 14:21:17 -0700

    adding new scripts for rthe google sync drive command

--- packages/shared/services/google-drive/index.ts
+++ packages/shared/services/google-drive/index.ts
@@ -5,8 +5,10 @@
 export { default as GoogleAuthService } from './google-auth-service';
 export { default as GoogleDriveService } from './google-drive-service';
 export { default as GoogleDriveSyncService } from './google-drive-sync-service';
+export { default as SourcesGoogleUpdateService } from './sources-google-update-service';
 
 // Also export types
 export * from './google-auth-service';
 export * from './google-drive-service';
 export * from './google-drive-sync-service';
+export * from './sources-google-update-service';
